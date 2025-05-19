53986a928ec4e080d8c3b38ffdf8bcabe1356c21
Cardoso <2231592@my.ipleiria.pt>
2025-05-19 11:41:21 +0100

    bug fixes

--- assets/js/fancybox-jpstudio-debug.js
+++ assets/js/fancybox-jpstudio-debug.js
@@ -4,8 +4,8 @@ $(document).ready(async function () {
 
   const day = 1;
   const imageCount = 116;
-  const cdnBase = 'https://jpcdn-sd.xperia.pt.xperia.pt';
-  const cdnHD = 'https://jpcdn-sd.xperia.pt.xperia.pt';
+  const cdnBase = 'https://jpcdn-sd.xperia.pt';
+  const cdnHD = 'https://jpcdn-sd.xperia.pt';
 
   const isMobile = /Mobi|Android/i.test(navigator.userAgent);
   const preloadCount = isMobile ? 4 : 10;
